1c038118a75cec58205f067115bb12f95e1ba0d0
Lynden Lim <lyndenlim15@gmail.com>
2022-02-23 16:35:51 -0500

    commit tree b641d44c83405e3e3516e8d337b2458d8b799dae

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+class Anagram
+  attr_reader :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(array)
+    modified_array = array.filter.with_index { |element, index|
+      if element.split("").sort == @word.split("").sort
+        array[index]
+      end
+    }
+    modified_array
+  end
+end
+
